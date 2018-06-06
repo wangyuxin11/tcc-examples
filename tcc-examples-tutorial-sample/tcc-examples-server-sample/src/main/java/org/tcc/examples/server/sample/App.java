b1d7ee3e659f636e5cb291c32250387b6d17fcba
wangyuxin <youngmanpoorman@163.com>
2018-06-06 16:26:18 +0800

    g

---
+++ tcc-examples-tutorial-sample/tcc-examples-server-sample/src/main/java/org/tcc/examples/server/sample/App.java
@@ -0,0 +1,13 @@
+package org.tcc.examples.server.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
